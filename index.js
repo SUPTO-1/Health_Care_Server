95a5b280512ebd0d171b5f42cfafe96de254a325
SUPTO-1 <jahidulislamsupta9@gmail.com>
2024-06-20 14:01:09 +0600

    user can delete a test

--- index.js
+++ index.js
@@ -113,6 +113,13 @@ async function run() {
       const result = await testCollection.updateOne(filter,updatedDoc);
       res.send(result);
     })
+
+    app.delete('/test/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await testCollection.deleteOne(query);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
